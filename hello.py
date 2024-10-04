ecc07d14e03a623b555c8bdc9ae563e1676baf37
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2024-10-04 01:23:59 +0000

    Atividade Semana_02 Feita

--- hello.py
+++ hello.py
@@ -4,52 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<p>Hello from Flask From Python and NOW ON GIT HUB!</p><table><tr><td><b>Aluno:</b></td><td>Guilherme Matheus de Jesus de Araujo</td></tr><tr><td><b>Prontuário:</b></td><td>PT3026841</td></tr></table>'
+    return '<h1>Avaliação contínua: Aula 030</h1><br><ul><li><a href="/">Home</a></li><li><a href="/user/GuiMatheus/PT3026841/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></u>'
 
 
-@app.route('/user/<name>')
-def hello_world2(name):
-    return '<p>Hello from Flask!</p><table><tr><td><b>Aluno:</b></td><td>{}</td></tr><tr><td><b>Prontuário:</b></td><td>PT3026841</td></tr></table>'.format(name)
-"""
-teste
-@app.route('/contextoderequisicao')
-def contexto():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>{}</p>'.format(user_agent)
 
-@app.route('/codigostatusdiferente')
-def statuscode():
-    response = make_response('<p>Bad Request</p>', 400)
-    return response
+@app.route('/user/<name>/<pront>/<inst>')
+def hello_pront(name, pront, inst):
+    return '<h1>Dados pela URL!</h1><table><tr><td><b>Aluno:</b></td><td>{}</td></tr><tr><td><b>Prontuário:</b></td><td>{}</td></tr><td><b>Instituição:</b></td><td>{}</td></tr></table>'.format(name, pront, inst)
 
-@app.route('/objetoresposta')
-def objectresponse():
-    response = make_response('<p>Oi, tem um cookie em mim mesmo</p>', 400)
-    response.set_cookie("chave", "35")
-    return response
-
-#EXEMPLO DE FAZER UM COOKIE
-
-resp.set_cookie(
-        'meu_cookie',
-        'valor_do_cookie',
-        max_age=60*60*24,  # 1 dia
-        expires='Fri, 31-Dec-2023 23:59:59 GMT',  # Data específica
-        path='/',
-        secure=True,
-        httponly=True,
-        samesite='Lax'
-    )
-
-
-@app.route('/redirecionamento')
-def redirection():
-    return redirect('https://ptb.ifsp.edu.br/')
-
-@app.route('/abortar')
-def aborta():
-    return abort(403)
-"""
-
+@app.route('/contextorequisicao')
+def hello_requisi_detalhes():
+    navegador = request.headers.get('User-Agent')
+    Ip_remoto = request.headers.get('X-Forwarded-For')
+    host_name = request.headers.get('Host')
+    return '<h1>Dados pelo Contexto!</h1>Browser: {}</p><br><p>IP_cliente: {}</p><br><p>Host: {}</p>'.format(navegador,Ip_remoto,host_name)
 
 
