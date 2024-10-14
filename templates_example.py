03c80c0d563f6ef3070cebb61e77b13534e8898a
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2024-10-14 19:01:38 +0000

    Treino de templates com Jinja2

---
+++ templates_example.py
@@ -0,0 +1,25 @@
+# A very simple Flask Hello World app for you to get started with...
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    name = "eu estou usando o JINJA2!";
+    return render_template('indexReal.html');
+
+
+@app.route('/user/<name>/<pront>/<inst>')
+def hello_pront(name, pront, inst):
+    return '<h1>Dados pela URL!</h1><table><tr><td><b>Aluno:</b></td><td>{}</td></tr><tr><td><b>Prontuário:</b></td><td>{}</td></tr><td><b>Instituição:</b></td><td>{}</td></tr></table>'.format(name, pront, inst)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404;
+
+@app.route('/contextorequisicao')
+def hello_requisi_detalhes():
+    navegador = request.headers.get('User-Agent')
+    Ip_remoto = request.headers.get('X-Forwarded-For')
+    host_name = request.headers.get('Host')
+    return '<h1>Dados pelo Contexto!</h1>Browser: {}</p><br><p>IP_cliente: {}</p><br><p>Host: {}</p>'.format(navegador,Ip_remoto,host_name)
