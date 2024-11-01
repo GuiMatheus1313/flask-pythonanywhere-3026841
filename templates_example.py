f57444838b09b8ecd23e6e1f521b470d110b8b8f
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2024-11-01 20:55:50 +0000

    Modificacao de Session com Forms

--- templates_example.py
+++ templates_example.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from flask_moment import Moment
 #Parte do WTF
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -19,6 +19,9 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('Qual teu nome?', validators = [DataRequired()])
+    sobreNome = StringField('Qual teu sobrenome?', validators = [DataRequired()])
+    inst = StringField('Informe a sua Insituição de ensino:', validators = [DataRequired()])
+    disciplina = SelectField('Informe a sua disciplina', choices=[('Desenvolvimento Web: Servidor', 'DWEBS'), ('Gestão de TI', 'GSTI'), ('Projeto de Extensão 4', 'EX4')])
     submit = SubmitField('Submit')
 
 #Essa rota está para uso do forms
@@ -26,12 +29,16 @@ class NameForm(FlaskForm):
 def hello_world():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Mudando nome!')
+        session['navegador'] = request.headers.get('User-Agent')
+        session['Ip_remoto'] = request.headers.get('X-Forwarded-For')
+        session['host_name'] = request.headers.get('Host')
         session['name'] = form.name.data
+        session['sobreNome'] = form.sobreNome.data
+        session['inst'] = form.inst.data
+        session['disciplina'] = form.disciplina.data
         return redirect(url_for('hello_world'))
-    return render_template('formularioTeste.html', form = form, name = session.get('name'))
+    return render_template('formularioTeste.html', form = form, name = session.get('name'), sobreNome = session.get('sobreNome'), inst = session.get('inst'), disciplina = session.get('disciplina'),
+    browser = session.get('navegador'), ip_remoto = session.get('Ip_remoto'), host_name = session.get('host_name'), current_time = datetime.utcnow())
 """
 @app.route('/')
 def hello_world():
