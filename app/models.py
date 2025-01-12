122fefd0df15a2f0fe0a5ff227c8ebd7f339af99
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2025-01-12 15:07:45 +0000

    Tentativa de transicionar novo modelo de dados (falha)

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
-from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flask import current_app
+from flask_login import UserMixin
+from . import db, login_manager
 
 
 class Role(db.Model):
@@ -10,11 +14,50 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique = True, index = True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
+    # Criação de uma propriedade somente para leitura chamada password
+    # Tentativa de ler a propriedade password resultará em um erro
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    # Quando a propriedade password for definida, o método setter chamará a função generate_password_hash
+    # e escreverá o resultado no campo password_hash
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # Compara senha fornecida pelo usuário com o hash armazenado
+    # Se True, senha correta
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
     def __repr__(self):
         return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
